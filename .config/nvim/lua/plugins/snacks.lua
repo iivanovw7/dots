f37b5f696b53c99b8a4102e6e1fe23dd7d301833
Igor Ivanov <igivanov@devexperts.com>
2026-08-21 10:03:28 +0100

    feat: :sparkles: updates snacks explorer config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -7,11 +7,46 @@ return {
 		opts = {
 			bigfile = { enabled = true },
 			dashboard = { enabled = true },
-			-- image = { enabled = true },
 			explorer = { enabled = true },
 			indent = { enabled = true },
 			input = { enabled = true },
+			image = { enabled = true },
+			toggle = { enabled = true },
+			git = { enabled = true },
 			picker = {
+				actions = {
+					expand_explorer = function(picker)
+						local win = vim.api.nvim_get_current_win()
+
+						if not picker._explorer_original_width then
+							picker._explorer_original_width = vim.api.nvim_win_get_width(win)
+						end
+
+						if picker._explorer_expanded then
+							vim.api.nvim_win_set_width(win, picker._explorer_original_width)
+
+							picker._explorer_expanded = false
+							return
+						end
+
+						local item = picker:current()
+						if not item then
+							return
+						end
+
+						local path = item.file or item.text or ""
+						local filename = vim.fn.fnamemodify(path, ":t")
+
+						local wanted_width = vim.fn.strdisplaywidth(filename) + 30
+
+						vim.api.nvim_win_set_width(win, wanted_width)
+						picker._explorer_expanded = true
+					end,
+
+					explorer = {
+						desc = "Expand Explorer",
+					},
+				},
 				enabled = true,
 				sources = {
 					explorer = {
@@ -20,12 +55,22 @@ return {
 						exclude = {
 							".git",
 						},
+						win = {
+							list = {
+								keys = {
+									["e"] = {
+										"expand_explorer",
+										mode = { "n", "x" },
+										desc = "Expand Explorer to filename",
+									},
+								},
+							},
+						},
 					},
 				},
 			},
 			notifier = { enabled = true },
 			quickfile = { enabled = true },
-			toggle = { enabled = true },
 			-- scope = { enabled = true },
 			-- scroll = { enabled = true },
 			statuscolumn = { enabled = true },
